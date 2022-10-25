b551f01cf0af7fe8a481d7c30d2e93445db05560
Ismael Miranda <ismael@ismael.com>
2022-10-24 23:35:59 -0300

    Exemplo de POO aula2b - curso em video

---
+++ src/poo/aula02/Caneta.java
@@ -0,0 +1,33 @@
+package poo.aula02;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void rabiscar(){
+        if(this.tampada == true){
+            System.out.println("ERRO! Não possso rabiscar.Estou tampada");
+        }else{
+            System.out.println("Estou rabiscando");
+        }
+    }
+
+    void tampar(){
+        this.tampada = true;
+    }
+
+    void destampar(){
+        this.tampada = false;
+    }
+
+    void status(){
+        System.out.println("Modelo " + this.modelo);
+        System.out.println("Uma caneta: " + this.cor);
+        System.out.println("Ponta " + this.ponta);
+        System.out.println("Carga " + this.carga);
+        System.out.println("Esta tampada? " + this.tampada);
+    }
+}
